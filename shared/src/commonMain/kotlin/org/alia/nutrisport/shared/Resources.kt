6d362516aa4ad729e2ed07c206dc23bb44e16694
Alejandro Salazar <salazar.cruz.alejandro@gmail.com>
2026-01-08 11:39:42 -0600

    - first commit

---
+++ shared/src/commonMain/kotlin/org/alia/nutrisport/shared/Resources.kt
@@ -0,0 +1,57 @@
+package org.alia.nutrisport.shared
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowLeft
+import androidx.compose.material.icons.automirrored.filled.ArrowRight
+import androidx.compose.material.icons.automirrored.filled.Login
+import androidx.compose.material.icons.automirrored.filled.Logout
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.filled.Category
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.GifBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Money
+import androidx.compose.material.icons.filled.Park
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PinDrop
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.VerticalSplit
+
+object Resources {
+    object Icon {
+        val Plus = Icons.Default.Add
+        val Minus = Icons.Default.Remove
+        val SignIn = Icons.AutoMirrored.Default.Login
+        val SignOut = Icons.AutoMirrored.Default.Logout
+        val Unlock = Icons.Default.Lock
+        val Search = Icons.Default.Search
+        val Person = Icons.Default.Person
+        val CheckMark = Icons.Default.Check
+        val Edit = Icons.Default.Edit
+        val Menu = Icons.Default.Menu
+        val BackArrow = Icons.AutoMirrored.Default.ArrowLeft
+        val RightArrow = Icons.AutoMirrored.Default.ArrowRight
+        val Home = Icons.Default.Home
+        val ShoppingCart = Icons.Default.ShoppingCart
+        val Categories = Icons.Default.Category
+        val Dollar = Icons.Default.Money
+        val MapPin = Icons.Default.PinDrop
+        val Close = Icons.Default.Close
+        val Book = Icons.Default.Book
+        val VerticalMenu = Icons.Default.VerticalSplit
+        val Delete = Icons.Default.Delete
+        val Google = Icons.Default.GifBox
+        val PayPal = Icons.Default.Park
+    }
+    object Image {
+
+    }
+}
